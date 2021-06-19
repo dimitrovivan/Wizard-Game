eec3b903e1dfd78e75b21f9222750b67ea469312
Ivan Dimitrov <bgivandimitrov@gmail.com>
2021-06-19 17:48:10 +0300

    Added keydown and keyup handlers

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,42 @@
 const startBtn = document.querySelector('.game-screen__start-btn');
+startBtn.addEventListener('click', onStartGame);
 
-function startGame(e) {
+const keys = {};
+const keyboardConfig = {
+    moveForward: 'w',
+    moveDown: 's',
+    moveLeft: 'a',
+    moveRight: 'd'
+}
+const validKeys = [];
+
+function onStartGame(e) {
     e.preventDefault();
     e.target.classList.add('hide');
+    saveUserKeyboardConfig();
+    window.requestAnimationFrame(runOnFrame);
 }
-startBtn.addEventListener('click', startGame);
 
+function saveUserKeyboardConfig() {
+    Object.values(keyboardConfig).map(key => validKeys.push(key));
+}
+
+function IsValidKeyPressed(e) {
+   return validKeys.find(validKey => e.key == validKey) ? true : false;
+}
+
+function runOnFrame(timestamp) {
+    console.log(keys);
+    window.requestAnimationFrame(runOnFrame);
+}
+
+function onKeyDownSaveCode(e) {
+    if(IsValidKeyPressed(e)) keys[e.key] = true;
+}
+
+function onKeyUpDeleteCode(e) {
+    if(IsValidKeyPressed(e)) keys[e.key] = false;
+}
+
+window.addEventListener('keydown', onKeyDownSaveCode);
+window.addEventListener('keydown', onKeyUpDeleteCode);
