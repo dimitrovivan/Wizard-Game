b5b5a8dae2a335908bc9e6594c3bb280e72c4dfb
Ivan Dimitrov <bgivandimitrov@gmail.com>
2021-06-20 09:01:33 +0300

    Clear input keyboard setting on click - better UI

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
 const startSection = document.querySelector('.start-section');
+startSection.addEventListener('click', onClearInputValue);
+
 const startBtn = document.querySelector('.start-section__btn');
 startBtn.addEventListener('click', onStartGame);
+
+const keys = {};
+const validKeys = [];
 
 window.addEventListener('keydown', onKeyDownSaveCode);
 window.addEventListener('keydown', onKeyUpDeleteCode);
@@ -8,7 +13,6 @@ window.addEventListener('keydown', onKeyUpDeleteCode);
 function onStartGame(e) {
     e.preventDefault();
     startSection.classList.add('hide');
-    saveUserKeyboardConfig();
     window.requestAnimationFrame(runOnFrame);
 }
 
@@ -16,19 +20,10 @@ function runOnFrame(timestamp) {
     window.requestAnimationFrame(runOnFrame);
 }
 
-const keys = {};
-
-const validKeys = [];
-
-const keyboardConfig = {
-    moveForward: 'w',
-    moveDown: 's',
-    moveLeft: 'a',
-    moveRight: 'd'
-}
-
-function saveUserKeyboardConfig() {
-    Object.values(keyboardConfig).map(key => validKeys.push(key));
+function onClearInputValue(e) {
+    if(e.target.tagName == "INPUT") e.target.value = "";
+    else return;
+    
 }
 
 function IsValidKeyPressed(e) {
@@ -42,5 +37,3 @@ function onKeyDownSaveCode(e) {
 function onKeyUpDeleteCode(e) {
     if(IsValidKeyPressed(e)) keys[e.key] = false;
 }
-
-
